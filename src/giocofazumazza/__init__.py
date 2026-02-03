c10044176b35e2795d85cc9cd409fff9c5d94700
AREyouSuRE? <gabrymazza2009@gmail.com>
2026-02-03 09:33:09 +0100

    ciaoooo

--- src/giocofazumazza/__init__.py
+++ src/giocofazumazza/__init__.py
@@ -1,1 +1,33 @@
 def main() -> None:
+    import pygame
+
+    pygame.init()
+
+    screen = pygame.display.set_mode( (800, 600) )
+
+    pygame.display.set_caption("this game is too MASSIVE")
+
+    running = True
+
+    Titlefont = pygame.font.SysFont('Impact', 100)
+    Normalfont = pygame.font.SysFont('Impact', 30)
+
+    game_end = Titlefont.render("Hai Perso", True, "red")
+    close_tip = Normalfont.render("Click ESC to exit", True, "blue","yellow")
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    
+        pygame.display.flip()
+
+
+    screen.blit(game_end, (100,100))
+    screen.blit(close_tip, (100,300))
+        
+    pygame.quit()
+                
